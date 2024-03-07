2b173a1d95e5d0df91cd6a1863b32c0b794c77bb
Elie Mauffrey <emauffrey@yahoo.fr>
2024-03-07 18:04:47 +0100

    PIVOT-8953 Fix scripts

--- generateMapping.sh
+++ generateMapping.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Launch MappingApplication main method (the end of the command line is meant to hide SLF4J messages)
-java -cp target/java-api-migration-tool-1.0-SNAPSHOT-jar-with-dependencies.jar com.activeviam.mapping.app.MappingApplication "$@" 2>&1 | egrep -v "(^SLF4J)"
+java -cp target/java-api-migration-tool-1.0-SNAPSHOT-jar-with-dependencies.jar com.activeviam.mapping.api.MappingApplication "$@" 2>&1 | egrep -v "(^SLF4J)"
