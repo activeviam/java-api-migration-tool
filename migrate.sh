e8e8f0216d315c2e47f3011ef61c5b0e64471393
Elie Mauffrey <emauffrey@yahoo.fr>
2024-03-07 19:37:54 +0100

    PIVOT-8953 Bump to version 1.1-SNAPSHOT

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Launch MigrationApplication main method
-java -cp target/java-api-migration-tool-1.0-jar-with-dependencies.jar com.activeviam.migration.api.MigrationApplication "$@"
+java -cp target/java-api-migration-tool-1.1-SNAPSHOT-jar-with-dependencies.jar com.activeviam.migration.api.MigrationApplication "$@"
